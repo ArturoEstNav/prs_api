376d8aa6110df1d9de4c29c858c7ca41ca22f1fa
Arturo Estrada <arturoestradanav@gmail.com>
2021-05-22 23:58:18 -0500

    Add lead cable, eq, wax potted, and output to pickups

--- db/migrate/20210517191727_create_pickups.rb
+++ db/migrate/20210517191727_create_pickups.rb
@@ -6,6 +6,12 @@ class CreatePickups < ActiveRecord::Migration[6.1]
       t.string :position, default: 'bridge'
       t.string :dc_resistance, default: '12k'
       t.string :magnet_type, default: 'alnico 4'
+      t.string :lead_cable, default: '4 conductor'
+      t.string :output, default: 'medium output'
+      t.integer :bass_eq, default: 0
+      t.integer :mids_eq, default: 0
+      t.integer :treble_eq, default: 0
+      t.boolean :wax_potted, default: true
 
       t.timestamps
     end
