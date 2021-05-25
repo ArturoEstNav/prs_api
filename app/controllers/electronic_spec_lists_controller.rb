6167926562f3808d4e1c627407ff445515c713c3
Arturo Estrada <arturoestradanav@gmail.com>
2021-05-25 16:19:57 -0500

    Add new electronic list form and controller logic for user additions

--- app/controllers/electronic_spec_lists_controller.rb
+++ app/controllers/electronic_spec_lists_controller.rb
@@ -1,13 +1,34 @@
 class ElectronicSpecListsController < ApplicationController
   def new
+    @guitar = Guitar.find(params[:guitar_id])
+    @electronic_spec_list = ElectronicSpecList.new
   end
 
   def create
+    @guitar = Guitar.find(params[:guitar_id])
+    @electronic_spec_list = ElectronicSpecList.new(electronic_params)
+    @electronic_spec_list.guitar = @guitar
+    if @electronic_spec_list.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def electronic_params
+    params.require(:electronic_spec_list).permit(
+      :switch_type,
+      :electronic_list,
+      :capacitor_values,
+      :potentiometer_values
+    )
   end
 end
