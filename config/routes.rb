189eeef95df64c12dffc35d07140eeca9b7c412b
Arturo Estrada <arturoestradanav@gmail.com>
2021-05-24 20:11:30 -0500

    Update routes for user contributions

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   get 'pages/documentation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bridges, only: [:new, :create, :edit, :update]
-  resources :bodies, only: [:new, :create, :edit, :update]
-  resources :electronic_spec_lists, only: [:new, :create, :edit, :update]
-  resources :guitars, only: [:new, :create, :edit, :update]
-  resources :necks, only: [:new, :create, :edit, :update]
+  resources :finishes, only: [:new, :create, :edit, :update]
   resources :pickups, only: [:new, :create, :edit, :update]
   resources :tuners, only: [:new, :create, :edit, :update]
+  resources :guitars, only: [:new, :create, :edit, :update] do
+    resources :necks, only: [:new, :create, :edit, :update]
+    resources :bodies, only: [:new, :create, :edit, :update]
+    resources :electronic_spec_lists, only: [:new, :create, :edit, :update]
+  end
 end
